bdbbf1c53c46c8029e0bcbb84ab2cf423b410572
liulujia <liulujia@jimi360.cn>
2019-04-09 17:48:45 +0800

    4

--- JMaxBrowser.js
+++ JMaxBrowser.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {requireNativeComponent,NativeModules} from 'react-native';
+import {requireNativeComponent,NativeModules,View} from 'react-native';
 
 export default class JMImageBrowserSreen extends React.Component {
     static propTypes = {
